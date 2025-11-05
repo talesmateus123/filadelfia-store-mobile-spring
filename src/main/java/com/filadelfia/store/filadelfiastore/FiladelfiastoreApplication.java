dd67ad8a1e6e45d5ed8d2fbd54f74e06758bca32
tales.ferreira <talesmateus1999@hotmail.com>
2025-11-04 21:18:29 -0300

    First commit, initialized spring project

---
+++ src/main/java/com/filadelfia/store/filadelfiastore/FiladelfiastoreApplication.java
@@ -0,0 +1,13 @@
+package com.filadelfia.store.filadelfiastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiladelfiastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiladelfiastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filadelfia/store/filadelfiastore/FiladelfiastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filadelfia.store.filadelfiastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiladelfiastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
